a1ac2da18b29c57ffd65f264e563efd3d5c731d4
fukicycle <lego.sato.4135@gmail.com>
2024-11-15 15:35:28 +0900

    bug fix

---
+++ Chat.SampleApp/wwwroot/js/checkNotification.js
@@ -0,0 +1,11 @@
+﻿function checkNotification() {
+    if (Notification.permission === 'granted') {
+        console.log('通知が許可されています');
+        return true;
+    } else if (Notification.permission === 'denied') {
+        console.log('通知が拒否されています');
+    } else {
+        console.log('通知の許可状態がまだ決まっていません');
+    }
+    return false;
+}
