cb09ca6b9c20dee16a04ddf2c87339b7700e7079
fukicycle <lego.sato.4135@gmail.com>
2024-11-15 10:57:26 +0900

    再読み込みを確認できるように実装

--- Chat.SampleApp/wwwroot/js/unload.js
+++ Chat.SampleApp/wwwroot/js/unload.js
@@ -1,4 +1,8 @@
 ﻿function unloadInitialize(dotnetObjectReference) {
+    window.addEventListener('beforeunload', (event) => {
+        event.preventDefault();
+        event.returnValue = "";
+    });
     window.addEventListener('unload', (event) => {
         dotnetObjectReference.invokeMethodAsync("Close");
     });
